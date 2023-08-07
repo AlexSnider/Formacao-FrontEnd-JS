6b74897c644a446b851a6bd236e4da3ba5b5a871
AlexSnider <alexsnider8@gmail.com>
2023-08-07 16:48:45 -0300

    1.0.0

---
+++ 3_ESTRUTURAS_DE_PROGRAMAÇÃO/js/script.js
@@ -0,0 +1,82 @@
+// let nome = "Alex";
+
+// console.log(nome);
+
+// nome = "Alex Oliveira";
+
+// console.log(nome);
+
+// const idade = 29;
+
+// console.log(idade);
+
+// console.log(Math.max(5, 2, 1, 10));
+
+// console.log(Math.floor(6.01));
+
+// console.log(Math.ceil(6.89));
+
+// const m = 10;
+
+// if (m > 5) {
+//   console.log("M é maior que 5");
+// }
+
+// const user = "jão";
+
+// if (user === "jão") {
+//   console.log(`é igual a ${user}`);
+// }
+
+// const loggendIn = false
+
+// if (loggendIn) {
+//   console.log('está autenticado');
+// } else {
+//   console.log('não está autenticado');
+// }
+
+// const q = 10
+// const w = 25
+
+// if (q > 5 && w > 20) {
+//   console.log('numeros mais altos');
+// } else {
+//   console.log('os numeros não são mais altos');
+// }
+
+// if (1 > 2) {
+//   console.log("teste");
+// } else if (2 > 3) {
+//   console.log("teste 2");
+// } else if (5 > 1) {
+//   console.log("agora sim");
+// }
+
+// const userName = 'Alex'
+// const userAge = 29
+
+// if (userName === 'who knows') {
+//   console.log(`bem vindo jose`);
+// } else if (userName === 'Alex' && userAge === 29) {
+//   console.log('bem vindo alex, voce tem 29 anos');
+// } else {
+//   console.log('nada aconteceu');
+// }
+
+// let p = 0;
+
+// while (p < 10) {
+//   console.log(`repetindo ${p}`);
+//   p = p + 1;
+// }
+
+for (let t = 0; t < 10; t++) {
+  console.log("repetindo");
+}
+
+let r = 10
+
+for (r; r > 0; r--) {
+  console.log(`o r está diminuindo ${r}`)
+}
